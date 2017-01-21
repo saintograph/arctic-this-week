97c836b2c5f1f059f5949b68c16c3c2ae8b7c686
Winfred Selwyn Ooh Azlin <winfredselwyn@gmail.com>
2017-01-21 22:06:13 +0800

    feat: Added Redux actions
    Moving the code to its own branch

---
+++ src/actions/action-get-news.js
@@ -0,0 +1,14 @@
+const ROOT_URL = 'http://www.mocky.io/v2/5185415ba171ea3a00704eed';
+
+
+export const LOAD_ALL_NEWS = 'LOAD_ALL_NEWS';
+
+
+export function loadAllNews() {
+  const request = fetch(ROOT_URL);
+
+  return {
+    type: LOAD_ALL_NEWS,
+    payload: request,
+  }
+}
